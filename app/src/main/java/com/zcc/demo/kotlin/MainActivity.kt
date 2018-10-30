78adcf319ddc7a82ce29159357637dc4535eca1d
zccneil <zccneil@163.com>
2018-10-30 16:33:33 +0800

    init

---
+++ app/src/main/java/com/zcc/demo/kotlin/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.zcc.demo.kotlin
+
+import android.content.Context
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mItems = listOf(
+            "a", "b", "c", "d", "e", "e"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv_message.text = "mesage of kotlin"
+        niceToast("hi")
+        rv_main_list.layoutManager = LinearLayoutManager(this)
+        rv_main_list.adapter = MyAdapter(mItems, this)
+    }
+
+    fun niceToast(message: String, className: String = "wtf???",
+                  length: Int = Toast.LENGTH_LONG): Unit {
+        Toast.makeText(this, "[$className] $message", length).show();
+    }
+
+    class MyAdapter(val items: List<String>, context: Context) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+        var context: Context = context
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(TextView(parent.context))
+        }
+
+        override fun getItemCount(): Int = items.size
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.bind(items[position])
+        }
+
+
+        class ViewHolder(itemView: TextView) : RecyclerView.ViewHolder(itemView) {
+            fun bind(s: String) {
+                (itemView as TextView).text = s
+            }
+
+        }
+
+    }
+}
